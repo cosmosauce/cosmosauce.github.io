fc3dc01681d3330e8d6ab9c5d991321de48adb2c
Not Set <wesley.ky.ho@gmail.com>
2024-08-05 12:43:08 +0800

    Import files

---
+++ index.js
@@ -0,0 +1,34 @@
+// Reddit Tip
+tippy('[reddit-tip]', {
+    content: 'u/Starry_Tacos',
+    placement: 'top',
+    arrow: 'round';
+    animation: 'shift-away'
+});
+
+function redditCopy() {
+    navigator.clipboard.writeText(u/Starry_Tacos);
+}
+
+// Mail Tip
+tippy('[mail-tip]', {}
+    content: 'wesley.ky.ho.app@gmail.com',
+    placement: 'top',
+    arrow: 'round';
+    animation: 'shift-away'
+});
+
+// Phone Tip
+tippy('[phone-tip]', {
+    content: '+852 9445 0278',
+    placement: 'top',
+    arrow: 'round';
+    animation: 'shift-away'
+});
+
+tippy('[twt-tip]', {
+    content: 'cosmo_sauce',
+    placement: 'top',
+    arrow: 'round';
+    animation: 'shift-away'
+});
