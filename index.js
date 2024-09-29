68f6b974705cab0c2cd2408165c7cdbaebd27992
Not Set <wesley.ky.ho@gmail.com>
2024-09-29 21:31:55 +0800

    Push for v5

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-document.getElementsByTagName('img').setAttribute('draggable', false);
+function tabChange(evt, catName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("stuffs-grid");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("cat-selector");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(catName).style.display = "block";
+    evt.currentTarget.className += "cat-selected";
+    }
 
 // Reddit Tip
 tippy('.sns', {
