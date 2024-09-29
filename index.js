3a9d92e47fcc1aa2d75c4a3850c7dfdcd96f4f68
Not Set <wesley.ky.ho@gmail.com>
2024-09-29 12:11:43 +0800

    Change stuff

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
+document.getElementsByTagName('img').setAttribute('draggable', false);
+
 // Reddit Tip
-tippy('[reddit-tip]', {
-    content: 'u/Starry_Tacos',
-    placement: 'top',
-    arrow: 'round';
-    animation: 'shift-away'
-});
-
-function redditCopy() {
-    navigator.clipboard.writeText(u/Starry_Tacos);
-}
-
-// Mail Tip
-tippy('[mail-tip]', {}
-    content: 'wesley.ky.ho.app@gmail.com',
-    placement: 'top',
-    arrow: 'round';
-    animation: 'shift-away'
-});
-
-// Phone Tip
-tippy('[phone-tip]', {
-    content: '+852 9445 0278',
-    placement: 'top',
-    arrow: 'round';
-    animation: 'shift-away'
-});
-
-tippy('[twt-tip]', {
-    content: 'cosmo_sauce',
-    placement: 'top',
-    arrow: 'round';
-    animation: 'shift-away'
+tippy('.sns', {
+    theme: 'light',
+    placement: 'bottom',
+    animation: 'shift-away-subtle',
 });
